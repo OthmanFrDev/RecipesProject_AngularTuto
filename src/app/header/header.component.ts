7fc71610d68d3c7e0e8a73e672ec46497a27357c
othman <othmanfroukh@gmail.com>
2021-11-20 23:56:54 +0100

    First commit in this project

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-header",
+    templateUrl: "./header.component.html",
+    styleUrls: []
+})
+export class HeaderComponent implements OnInit {
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
