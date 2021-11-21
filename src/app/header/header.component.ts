94b389915d2d10dcd729b3203fa1a2262e9fa98d
othman <othmanfroukh@gmail.com>
2021-11-21 14:42:23 +0100

    finishing first chapter Course Project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: []
 })
 export class HeaderComponent implements OnInit {
+    collapsed = true;
     ngOnInit(): void {
         throw new Error("Method not implemented.");
     }
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe("Spagetti","this is spagetti","https://assets.afcdn.com/recipe/20180326/78166_w1024h1024c1cx2592cy1728.jpg"),
+    new Recipe("Spagetti","this is spagetti","https://assets.afcdn.com/recipe/20180326/78166_w1024h1024c1cx2592cy1728.jpg"),
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
